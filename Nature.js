62b3f910a06320697e17c892402583c9373928ee
alijon <alijon.kurbanov.99@mail.ru>
2021-03-23 20:47:35 +0700

    deleted two last images

--- Nature.js
+++ Nature.js
@@ -10,13 +10,5 @@ every[1].addEventListener('click', function(err2) {
 every[2].addEventListener('click', function(err3) {
   err3 = "Природа/Nature2.jpg";
   document.getElementById('mainImg').src = err3;
-});
-every[3].addEventListener('click', function(err4) {
-  err4 = "Природа/Nature3.jpg";
-  document.getElementById('mainImg').src = err4;
-});
-every[4].addEventListener('click', function(err5) {
-  err5 = "Природа/Nature4.jpg";
-  document.getElementById('mainImg').src = err5;
 })
 
